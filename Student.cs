224dedb6f6c6742135f925703017925739dcfab4
James Dai <K038246@eltham-college.org.uk>
2021-02-01 11:46:00 +0000

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_Oriented_Programming_Students
+{
+    class Student
+    {
+        // Create values
+        private string FullName;
+        private int Age, CSScore;
+
+        // Default constructor
+        public Student()
+        {
+            FullName = "";
+            Age = 0;
+            CSScore = 0;
+        }
+
+        // Full constructor
+        public Student(string name, int age, int score)
+        {
+            FullName = name;
+            Age = age;
+            CSScore = score;
+        }
+
+        // Personal constructor
+        public Student(string name, int age)
+        {
+            FullName = name;
+            Age = age;
+            CSScore = 0;
+        }
+
+        //Print name
+        public void PrintName()
+        {
+            Console.WriteLine("Name: " + FullName);
+        }
+
+        //Print Age
+        public void PrintAge()
+        {
+            Console.WriteLine("Age: " + Age);
+        }
+
+        //Print score
+        public void PrintScore()
+        {
+            Console.WriteLine("Computer Science score: " + CSScore);
+        }
+
+        //Print personal details
+        public void PrintPersonalDetails()
+        {
+            PrintName();
+            PrintAge();
+        }
+
+        //Print all details
+        public void PrintAllDetails()
+        {
+            PrintName();
+            PrintAge();
+            PrintScore();
+        }
+    }
+}
